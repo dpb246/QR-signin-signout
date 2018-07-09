bf2db1a86edf4729803be3368f84f5eab3cede47
dpb246 <dpb246@users.noreply.github.com>
2018-07-08 21:09:00 -0400

    PRE VBS WEEK
    Final Build Pre VBS week

--- RTQR.py
+++ RTQR.py
@@ -1,5 +1,5 @@
 from pyzbar.pyzbar import decode
-from PIL import Image
+from PIL import Image, ImageTk
 import cv2
 import tkinter as tk
 import mysql.connector
@@ -10,52 +10,101 @@ import time
 
 database_name = 'VBS'
 
-
 def now():
     return datetime.datetime.now()
 def day():
     return calendar.day_name[now().weekday()]
-cnx = mysql.connector.connect(user='pma', password='YouW!llNeverGuess',host='127.0.0.1', database='VBSTesting')
+cnx = mysql.connector.connect(user='pma', password='YouW!llNeverGuess',host='127.0.0.1', database='VBS')
 cur = cnx.cursor(buffered=True)
-
 print("Init")
 status_action = {
     0:"Signed-In",
-    1:"Signed-out"
-}
+    1:"Signed-out"}
 status_short = {
     0:"In",
-    1:"Out"
-}
+    1:"Out"}
 opposite = {
     0: 1,
-    1: 0
-}
-def waitAndDecode(capture):
-    """
-    A simple function that captures webcam video utilizing OpenCV. The video is then broken down into frames which
-    are constantly displayed. The frame is then converted to grayscale for better contrast. Afterwards, the image
-    is transformed into a numpy array using PIL. This is needed to create zbar image. This zbar image is then scanned
-    utilizing zbar's image scanner and will then return the decodeed message of any QR or bar code.
-    :return:
-    """
-    # Begin capturing video. You can modify what video source to use with VideoCapture's argument. It's currently set
-    # to be your webcam.
-    while True:
-        # To quit this program press q.
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        # Breaks down the video into frames
-        ret, frame = capture.read()
-        # Displays the current frame
-        cv2.imshow('Current', frame)
+    1: 0}
+class VideoCapture:
+    def __init__(self, video_source=1):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+        # Get video source width and height
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+class VBSAttendance:
+    def __init__(self, window, window_title):
+        self.window = window
+        self.window.title(window_title)
+        self.window.columnconfigure(0, weight=1)
+        self.vid = VideoCapture()
+
+        self.canvas = tk.Canvas(window, width = self.vid.width/3, height = self.vid.height/3)
+        self.canvas.grid(row=2, column=0)
+
+        self.last_name = ""
+        self.delay = 10
+        self.title = tk.Label(self.window, text="VBS Sign-in/Sign-out", font=("Arial", 40))
+        self.title.grid(row=0, column=0)
+        self.label = tk.Label(self.window, text="Please place the QR Code for your kid below the camera\nFor help please see Katrina B", font=("Arial", 20))
+        self.label.grid(row=1, column=0)
+
+        self.app = tk.Label(self.window)
+        self.app.destroy()
+
+        self.update()
+        self.process()
+        self.window.mainloop()
+    def update(self):
+
+        self.data = None
+        ret, frame = self.vid.get_frame()
+        if ret:
+            self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
         # Converts image to grayscale.
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Uses PIL to convert the grayscale image into a ndary array that ZBar can understand.
         image = Image.fromarray(gray)
         d = decode(image)
+        self.window.after(self.delay, self.update)
         if d: #Only return once a valid QR code is decoded
-            return d[0].data.decode("utf-8")
+            self.data = d[0].data.decode("utf-8")
+    def process(self):
+        self.window.after(25, self.process)
+        if self.data and self.app.winfo_exists()==0:
+            who, name, group = decode_data(self.data)
+            if self.last_name==name: return
+            if who=="PARENT":
+                self.last_name = name
+                print(who, name, group)
+                current_status = get_status(name, group) #To get at their status need convert iter to a single number
+                print(current_status)
+                #Spawn a simple window to confirm the signin/signout
+                self.app = Comfirm(name, current_status, who, master=self.window)
+                self.app.grid(row=3, column=0)
+            elif who=="ADMIN":
+                print(who)
+                temp = tk.Frame()
+                self.app = Admin(master=self.window)
+                self.app.grid(row=3, column=0)
+                self.last_name = ""
 def get_status(name, group):
     cur.execute("SELECT Status FROM {} WHERE Name='{}' and Group_name='{}'".format(database_name, name, group))
     return list(cur)[0][0]
@@ -68,40 +117,6 @@ def change_status(name, who, status):
     cnx.commit()
 def decode_data(data):
     return data.split(":")
-def main():
-    capture = cv2.VideoCapture(0)
-    last_name = "" #don't want to get spammed with the current
-    Admin_last_frame = False #Sketchy
-    while True:
-        if cv2.waitKey(1) & 0xFF == ord('q'): #'q' to exit program
-            break
-        data = waitAndDecode(capture)
-        who, name, group = decode_data(data)
-        if last_name==name: continue
-        if who=="PARENT":
-            last_name = name
-            print(who, name, group)
-            current_status = get_status(name, group) #To get at their status need convert iter to a single number
-            print(current_status)
-            #Spawn a simple window to confirm the signin/signout
-            root = tk.Tk()
-            app = Comfirm(name, current_status, who, master=root)
-            app.mainloop()
-            Admin_last_frame = False
-        elif who=="ADMIN" and not Admin_last_frame:
-            Admin_last_frame = True
-            print(who)
-            root = tk.Tk()
-            app = Admin(master=root)
-            app.mainloop()
-            last_name = ""
-        else:
-            Admin_last_frame = False
-
-    #Close everything
-    cur.close()
-    cnx.close()
-    capture.release()
 def kill(buttons):
     for button in buttons:
         button.destroy()
@@ -109,35 +124,32 @@ class Admin(tk.Frame):
     #TODO: Make font size better and center on screen
     def __init__(self, master=None):
         super().__init__(master)
-        self.master.geometry("500x500")
-        self.master.resizable(0, 0)
-        self.pack(fill="both")
+        self.grid()
         self.master = master
-        self.group_names = ["Ducks", "Turtles", "Frogs"]
-        self.quit = tk.Button(self, text="Exit", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
-        self.label = tk.Label(self)
-        self.label.pack(fill=X, expand=1)
-        self.label["text"] = "Please select a group"
+        self.group_names = ["Ducks", "Turtles", "Frogs", "Strainers"]
+        self.quit = tk.Button(self, text="Exit", fg="red", command=self.destroy, font=("Arial", 14))
+        self.quit.grid(row=100)
+        self.label = tk.Label(self, text="Please select a group", font=("Arial", 10))
+        self.label.grid(row=2)
         self.groups()
     #TODO: make buttons bigger
     def groups(self):
         self.group_buttons = list()
-        for group in self.group_names:
-            self.group_buttons.append(tk.Button(master=self, text=group, command= lambda g=group:self.get_group(g)))
-            self.group_buttons[-1].pack(side="top")
+        for r, group in enumerate(self.group_names):
+            self.group_buttons.append(tk.Button(master=self, text=group, command= lambda g=group:self.get_group(g), font=("Arial", 12)))
+            self.group_buttons[-1].grid(row=r+3)
     def get_group(self, group):
         self.kid_buttons = list()
         print(group, get_kids(group))
         self.total_signed_in = 0
         for id, kid in enumerate(get_kids(group)):
             print(kid)
-            self.kid_buttons.append(tk.Button(master=self, text="Sign-" + status_short[kid[1]] + ": " + kid[0], command= lambda n=kid[0], s=kid[1], i=id: self.changeStatus(n, s, i)))
+            self.kid_buttons.append(tk.Button(master=self, text="Sign-" + status_short[kid[1]] + ": " + kid[0], command= lambda n=kid[0], s=kid[1], i=id: self.changeStatus(n, s, i), font=("Arial", 10)))
             self.total_signed_in += kid[1]
-            self.kid_buttons[-1].pack(side="top")
+            self.kid_buttons[-1].grid(row=id+3)
         self.label["text"] = "{}\nHas {} people signed in right now.".format(group, self.total_signed_in)
-        self.back = tk.Button(master=self, text="Back",  fg="blue", command=self.uplevel)
-        self.back.pack(side="bottom")
+        self.back = tk.Button(master=self, text="Back",  fg="blue", command=self.uplevel, font=("Arial", 14))
+        self.back.grid(row=99)
         kill(self.group_buttons)
         self.current_active_group = group
     def uplevel(self):
@@ -158,28 +170,28 @@ class Comfirm(tk.Frame):
     #TODO: Make font size better and center on screen
     def __init__(self, name, status, who, master=None):
         super().__init__(master)
-        self.master.geometry("500x500")
-        self.master.resizable(0, 0)
-        self.pack(fill="both")
+        self.grid()
         self.master = master
         self.name = name
         self.status = status
         self.action = status_action[status]
         self.who = who
         self.create_widgets()
-    #TODO: make buttons bigger
     def create_widgets(self):
-        self.label = tk.Label(self, text="{} is about to be {}.  Is this correct?".format(self.name, self.action))
-        self.label.pack(fill=X, expand=1)
-        self.yes = tk.Button(self)
-        self.yes["text"] = "YES"
-        self.yes["command"] = self.changeStatus
-        self.yes.pack(side="top")
+        self.label = tk.Label(self, text="{} is about to be {}.  Is this correct?".format(self.name, self.action), font=("arial", 30))
+        self.label.grid(row=2)
+        self.label2 = tk.Label(self, text="Hit space to confirm", font=("arial", 30))
+        self.label2.grid(row=6)
+        self.yes = tk.Button(self, font=("arial", 30), text="YES", command=self.changeStatus, fg="green")
+        self.yes.grid(row=3)
         self.yes.focus()
-        self.quit = tk.Button(self, text="NO", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.bind('<space>', (lambda e, s=self: s.changeStatus()))
+        self.quit = tk.Button(self, text="NO", fg="red", command=self.destroy, font=("arial", 30))
+        self.quit.grid(row=4)
     def changeStatus(self):
         change_status(self.name, self.who, self.status)
-        self.master.destroy()
+        self.destroy()
 
-main()
+VBSAttendance(tk.Tk(), "VBS")
+cur.close()
+cnx.close()
